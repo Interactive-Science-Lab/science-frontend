6a05cbbcf0476239089dc7622f18e88027aad1bd
jordan hubbard <thejhubbs@gmail.com>
2021-01-21 15:25:02 -0500

    demo link

--- src/project/lab/sidebar.js
+++ src/project/lab/sidebar.js
@@ -87,7 +87,7 @@ class Header extends React.Component {
                 
                 {
                 items.sort((a,b) => a.experiment_order - b.experiment_order).map((i, x) => 
-                <Link key={JSON.stringify(i) + x} className="experimentListName"  to={`/lab/${i.experiment_id}?l=${this.props.location.search.split('=')[1] || 'chemistry'}`}  
+                <Link key={JSON.stringify(i) + x} className="experimentListName"  to={`/lab/${i.experiment_id}?l=${this.props.location.search.split('=')[1] || 'demo'}`}  
                 style={{ display: 'block', padding: '15px', boxShadow: '2px 2px 4px black' }}>
 <span className="fas fa-caret-right" style={{color: 'white', display: 'block', position: 'absolute', right:'15px', opacity: '.2'}}></span>
 <h5 style={{ marginTop: "0", textAlign: 'left', textDecoration: 'none' }}><span style={{ color: "grey"}}>#{x+1}</span> <span style={{ color: "white"}}>{i.experiment_name}</span> </h5>
